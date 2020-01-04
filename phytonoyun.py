01a52d05592e2621300328083f0a3553c609af59
Ardoz <59261688+Ardoz@users.noreply.github.com>
2020-01-04 21:55:41 +0300

    Add files via upload

---
+++ phytonoyun.py
@@ -0,0 +1,98 @@
+#Turtle oyun
+import turtle
+import math
+import random
+
+
+ekran = turtle.Screen()
+ekran.bgcolor("black")
+ekran.tracer(3)
+
+sinir = turtle.Turtle()
+sinir.color("white")
+sinir.penup()
+sinir.setposition(-300,-300)
+sinir.pendown()
+sinir.pensize(3)
+for side in range(4):
+    sinir.forward(600)
+    sinir.left(90)
+sinir.hideturtle()
+
+oyuncu = turtle.Turtle()
+oyuncu.color("blue")
+oyuncu.shape("triangle")
+oyuncu.penup()
+oyuncu.speed(0)
+
+score = 0
+
+maxGoals = 10
+goals = []
+
+for count in range(maxGoals):
+    goals.append(turtle.Turtle())
+    goals[count].color("red")
+    goals[count].shape("circle")
+    goals[count].penup()
+    goals[count].speed(0)
+    goals[count].setposition(random.randint(-300, 300), random.randint(-300, 300))
+
+speed = 1
+
+def turnleft():
+    oyuncu.left(30)
+    
+def turnright():
+    oyuncu.right(30)
+    
+def increasespeed():
+    global speed
+    speed += 1
+
+def isCollision(t1, t2):
+    d = math.sqrt(math.pow(t1.xcor()-t2.xcor(),2) + math.pow(t1.ycor()-t2.ycor(),2))
+    if d < 20:
+        return True
+    else:
+        return False
+        
+
+turtle.listen()
+turtle.onkey(turnleft, "Left")
+turtle.onkey(turnright, "Right")
+turtle.onkey(increasespeed, "Up")
+
+while True:
+    player.forward(speed)
+    
+
+    if oyuncu.xcor() > 300 or oyuncu.xcor() < -300:
+        oyuncu.right(180)
+       
+
+    if player.ycor() > 300 or player.ycor() < -300:
+        player.right(180)
+       
+
+    for count in range(maxGoals):
+        goals[count].forward(3)
+
+       
+        if goals[count].xcor() > 290 or goals[count].xcor() < -290:
+            goals[count].right(180)
+            
+     
+        if goals[count].ycor() > 290 or goals[count].ycor() < -290:
+            goals[count].right(180)
+             
+
+        if isCollision(player, goals[count]):
+            goals[count].setposition(random.randint(-300, 300), random.randint(-300, 300))
+            goals[count].right(random.randint(0,360))
+          
+            score += 1
+
+
+
+delay = raw_input("Press Enter to finish.")
